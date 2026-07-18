7f7ea8c14daf84cc98802ecf4a68564372dd1ebe
TcnCunha <64099736+Tcncunha@users.noreply.github.com>
2026-07-18 23:37:20 +0200

    Modify words table schema and length data type
    Changed the table name to include schema and updated the length type from TINYINT to INT.

--- supabase/migrations/20250718_create_words_table.sql
+++ supabase/migrations/20250718_create_words_table.sql
@@ -3,10 +3,10 @@
 -- Descrição: Armazena palavras para o jogo Entre Linhas
 -- =============================================
 
-CREATE TABLE IF NOT EXISTS words (
+CREATE TABLE IF NOT EXISTS "CrossLinesGameDB"."words"  (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     word VARCHAR(20) NOT NULL,
-    length TINYINT NOT NULL,
+    length INT NOT NULL,
     language CHAR(2) NOT NULL,
     is_active BOOLEAN DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT NOW()
